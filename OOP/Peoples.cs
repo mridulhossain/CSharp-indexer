382c14dfb445cdb16e48c0d09d0a3d6046bcb1da
mridulhossain <mridulfeni94@gmail.com>
2021-09-18 12:14:44 +0600

    csharp indexer oop

---
+++ OOP/Peoples.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Peoples
+    {
+        private readonly List<People> PeopleList;
+
+        public Peoples()
+        {
+            PeopleList = new List<People>();
+        }
+
+        public void AddPeople(People people)
+        {
+            PeopleList.Add(people);
+        }
+
+        public People this[string name]
+        {
+            get
+            {
+                var people = PeopleList.Where(x => x.Name == name).FirstOrDefault();
+                return people;
+            }
+        }
+
+        public int TotalPeoplesCount()
+        {
+            return PeopleList.Count;
+        }
+     
+        public  List<People> TotalPeople()
+        {
+            return PeopleList;
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            People people1 = new People { Name = "Mridul", Age = 23, Address = "kalidaha,feni,chittagong" };
+            People people2 = new People { Name = "Bijoy", Age = 27, Address = "shitakunda,chittagong" };
+            People people3 = new People { Name = "sakib", Age = 27, Address = "jossore,bangladesh" };
+
+            Peoples peoples = new Peoples();
+            peoples.AddPeople(people1);
+            peoples.AddPeople(people2);
+            peoples.AddPeople(people3);
+
+            Console.WriteLine(peoples.TotalPeoplesCount());
+            peoples["Mridul"].Address = "kalidaha,feni,chittagong,bangladesh." ;
+            Console.WriteLine(peoples["Mridul"].Address);
+
+        }
+    }
+}
+
+
